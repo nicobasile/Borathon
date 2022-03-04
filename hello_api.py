51424d5d9bcc2ccc9db6e6190ce6a03348790012
nicobasile <nbasile@usc.edu>
2022-03-04 09:57:21 -0800

    Updated functions

--- hello_api.py
+++ hello_api.py
@@ -4,8 +4,8 @@ import uuid
 
 app = Flask(__name__)
 
-accounts = {} # map accountId : Account object
-customers = {} # map account number to customer object
+accounts = {} # map AccountNumber : Account object
+customers = {} # map AccountNumber to customer object
 
 # Accounts
 class AccountStatus(IntEnum):
@@ -78,14 +78,73 @@ def formatOpenedAccountResponse(customer, account):
         },
         'statusCode': 200
     }
+  
+  
+@app.route('/api/ApplyTransactionToCustomerAccountAsync', methods=['POST'])
+def applyTransaction():
+    body = request.get_json()
+    if 'accountNumber' not in body or 'amount' not in body or 'transactionType' not in body:
+        return jsonify({'statusCode': 400, 'Description': 'Invalid input of accountNumber/amount/transactionType'})
+
+    accountNumber, amount, transactionType = body['accountNumber'], body['amount'], body['transactionType']
+
+    #transaction type 1 == credit, 0 == debit
+    account = accounts[accountNumber]
+    
+    if account.AccountStatus == account.AccountStatus.CLOSED:
+      return jsonify({'statusCode': 400, 'Description': 'Bank account has been closed'})
+
+    if (transactionType == 1):
+        account.Balance += amount
+    elif (transactionType == 0):
+        if account.Balance - amount <= 0:
+            return jsonify({'statusCode': 400, 'Description': 'Insuffucient balance' })
+        else:
+            account.Balance -= amount
+    else:
+        return jsonify({'statusCode': 400, 'Description': 'Invalid transaction type, must be 0 for debit or 1 for credit' })
+
+    accounts[accountNumber] = account
+
+    return jsonify(formatApplyTransactionResponse(account))
+
+
+def formatApplyTransactionResponse(account):
+    customer = customers[account.AccountNumber]
+    return {
+        'value': {
+            'id': customer.ID,
+            'firstName': customer.firstName,
+            'lastName': customer.lastName,
+            'account': {
+                'id': account.ID,
+                'accountNumber': account.AccountNumber,
+                'balance': account.Balance,
+                'status': account.AccountStatus
+            },
+            "accountId": account.ID
+        },
+        'statusCode': 200
+    }
 
 @app.route('/api/CustomerDetails', methods=['GET'])
 def get_customer_details():
     acc_number = request.args.get('accnumber')
     if acc_number in customers:
         customer = customers[acc_number]
-        return 'First Name: ' + customer.firstName + ' Last Name: ' + customer.lastName + ' Associated Account = ' + customer.AssociatedAccount
+        return 'First Name: ' + customer.firstName + ' Last Name: ' + customer.lastName + ' Associated Account = ' + customer.AssociatedAccount + '\nBalance = ' + str(accounts[customer.AssociatedAccount].Balance)
     else:
         return 'No customer with that Account Number' + acc_number
+      
+@app.route('/api/CloseAccount', methods=['POST'])
+def close_account():
+    acc_number = request.args.get('accnumber')
+    if acc_number in accounts:
+        account = accounts[acc_number]
+        account.AccountStatus = AccountStatus.CLOSED
+        accounts[acc_number] = account
+        return 'Closed account number: ' + acc_number
+    else:
+        return 'No account with that Account Number: ' + acc_number
 
 app.run(use_reloader=True, debug=True)
